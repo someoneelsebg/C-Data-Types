f3b4089b18286c0c303065973d3a628c325618ae
someoneelsebg <someoneelsebg@outlook.com>
2023-01-24 20:09:43 +0200

    float-or-double

---
+++ 02.float-or-double.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    double x1 = 34.567839023;
+    float x2 = 12.345f;
+    double x3 = 8923.1234857;
+    float x4 = 3456.091f;
+    printf("%lf\n", x1);
+    printf("%f\n", x2);
+    printf("%lf\n", x3);
+    printf("%f\n", x4);
+    return 0;
+}
